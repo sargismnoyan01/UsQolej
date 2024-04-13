fc704143ec9b2edfc74a13b4bda0a92436e15954
Sargis <mnoyansargis@gmail.com>
2024-04-13 13:06:18 +0400

    end

--- UsQolej/settings.py
+++ UsQolej/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-62@dc1iruwujt78!#xk(df7%#cl6k0p-8tm#n4%j6wjqaw(w-e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*','13.48.43.163','www.vsc.am','vsc.am']
 
